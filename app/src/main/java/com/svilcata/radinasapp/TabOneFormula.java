97a54834cb59083ef82b32e6b47b43b1ed6c03be
Svilcata <jibryto1@gmail.com>
2016-12-08 01:46:06 +0200

    Initial commit

---
+++ app/src/main/java/com/svilcata/radinasapp/TabOneFormula.java
@@ -0,0 +1,20 @@
+package com.svilcata.radinasapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Svilcata on 07-Dec-16.
+ */
+
+public class TabOneFormula extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_formula_01, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/svilcata/radinasapp/TabTwoPainSupress.java
@@ -0,0 +1,20 @@
+package com.svilcata.radinasapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Svilcata on 07-Dec-16.
+ */
+
+public class TabTwoPainSupress extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_painsupress_02, container, false);
+        return rootView;
+    }
+}
